336015c845c42446636286e6fbcf3e655bac33a7
Warouxlucas <warouxlucas@hotmail.com>
2021-04-28 14:01:19 +0200

    uppdate

--- content/pages/Menu.md
+++ content/pages/Menu.md
@@ -6,7 +6,4 @@ featuredImage: https://ucarecdn.com/b9e675bc-b0c5-4125-8276-ea3517e4b49e/
 subtitle: ""
 ---
 lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
-
-
-
 loliloflro
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      Suivez-nous{' '}
+      <a href="https://instagram.com/thrivegoldcoast/">A Mani</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <p>Waroux Lucas</p>.
         </span>
       </div>
     </footer>
